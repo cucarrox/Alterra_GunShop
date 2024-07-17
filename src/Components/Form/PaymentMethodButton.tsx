53cc221fa1f05910b8df9c578704970bb7231eeb
cucarrox <brunocucarrox@gmail.com>
2024-07-17 11:44:25 -0300

    Pequenos ajustes nos botões de pagamento

--- src/Components/Form/PaymentMethodButton.tsx
+++ src/Components/Form/PaymentMethodButton.tsx
@@ -12,7 +12,7 @@ export const PaymentMethodButton = forwardRef(function PaymentMethodButton(
     <label
       data-state={isSelected}
       className={`flex justify-center items-center rounded-lg gap-2 p-3 px-5 w-1/3 text-xs bg-gray-200 border-2 active:bg-blue-100 active:border-blue-300 select-none cursor-pointer transition hover:bg-gray-300 hover:border-gray-400
-      ${isSelected ? "bg-blue-100 border-blue-300 hover:bg-blue-100 hover:border-blue-300" : ""}  
+      ${isSelected ? "bg-blue-200 border-blue-400 hover:bg-blue-200 hover:border-blue-400" : ""}  
       `}
     >
       <input className="hidden" type="radio" ref={ref} {...rest} />
